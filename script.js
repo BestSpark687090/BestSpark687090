c3be1d1cc23c8af82c19a088e80d3d2ec967caeb
BestSpark687090 <73793381+BestSpark687090@users.noreply.github.com>
2025-10-29 18:23:23 +0000

    Fix accidental removal of formatting when attempting to remove token

--- script.js
+++ script.js
@@ -7,21 +7,21 @@
 let urlToFetch = "https://api.github.com/repos/BestSpark687090/BestSpark687090/commits/main";
 let fetchingURL = "https://proxy.corsfix.com/?"
 let headers = {
-headers: {
-"Authorization": "{{API}}", // Taken care of by corsfix :)
-"Origin": location.host
-}
+  headers: {
+    "Authorization": "{{API}}", // Taken care of by corsfix :)
+    "Origin": location.host
+  }
 };
 (async () => {
-let res = await fetch(fetchingURL + urlToFetch, headers);
-let txt = await res.json(); // its gonna be in json
-let res2 = await fetch(fetchingURL + txt.url, headers);
-let txt2 = await res2.json();
-console.log(txt2.commit.message);
-let div = document.createElement("div");
-div.classList.add("latestCommitMessage");
-let span = document.createElement("span");
-span.innerText = "Latest Commit Message: " + txt2.commit.message;
-div.appendChild(span);
-document.body.appendChild(div);
+  let res = await fetch(fetchingURL + urlToFetch, headers);
+  let txt = await res.json(); // its gonna be in json
+  let res2 = await fetch(fetchingURL + txt.url, headers);
+  let txt2 = await res2.json();
+  console.log(txt2.commit.message);
+  let div = document.createElement("div");
+  div.classList.add("latestCommitMessage");
+  let span = document.createElement("span");
+  span.innerText = "Latest Commit Message: " + txt2.commit.message;
+  div.appendChild(span);
+  document.body.appendChild(div);
 })();
