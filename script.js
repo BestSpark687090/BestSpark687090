e71a82388edf198036f045132c95df9fd4489e2f
BestSpark687090 <73793381+BestSpark687090@users.noreply.github.com>
2025-10-29 18:21:05 +0000

    Display the latest commit message on the website (1)

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
 
 // link to get the commit stuff
 // raw: https://api.github.com/repos/BestSpark687090/BestSpark687090/commits/main
-// token i guess
-
+//https://proxy.corsfix.com/?
+// token i guess:
+// and yes im sure this is fine, because all it can do is access public repositories and stuff
 let urlToFetch = "https://api.github.com/repos/BestSpark687090/BestSpark687090/commits/main";
+let fetchingURL = "https://proxy.corsfix.com/?"
+let headers = {
+headers: {
+"Authorization": "{{API}}", // Taken care of by corsfix :)
+"Origin": location.host
+}
+};
 (async () => {
-  let res = await fetch("https://api.codetabs.com/v1/proxy?quest="+urlToFetch);
-  let txt = await res.json(); // its gonna be in json
-  let res2 = await fetch("https://api.codetabs.com/v1/tmp?quest="+txt.url);
-  let txt2 = await res2.json();
-  console.log(txt2.commit.message);
-  /**<div class="latestCommitMessage">
-    <span>Latest Commit Message: <p></p></span>
-  </div> */
-  let div = document.createElement("div");
-  div.classList.add("latestCommitMessage");
-  let span = document.createElement("span");
-  span.innerText = "Latest Commit Message: "+txt2.commit.message;
-  div.appendChild(span);
-  document.body.appendChild(div);
+let res = await fetch(fetchingURL + urlToFetch, headers);
+let txt = await res.json(); // its gonna be in json
+let res2 = await fetch(fetchingURL + txt.url, headers);
+let txt2 = await res2.json();
+console.log(txt2.commit.message);
+let div = document.createElement("div");
+div.classList.add("latestCommitMessage");
+let span = document.createElement("span");
+span.innerText = "Latest Commit Message: " + txt2.commit.message;
+div.appendChild(span);
+document.body.appendChild(div);
 })();
