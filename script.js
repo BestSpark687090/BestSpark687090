12295e0394e011cf39f4cd2a0f9a54c4550ccdd7
BestSpark687090 <73793381+BestSpark687090@users.noreply.github.com>
2025-11-07 17:53:50 +0000

    yea

--- script.js
+++ script.js
@@ -26,3 +26,14 @@ let headers = {
   div.appendChild(span);
   document.body.appendChild(div);
 })();
+async function fetched(body){
+  let res2 = await fetch(fetchingURL + body.body.url, headers);
+  let txt2 = await res2.json();
+  console.log(txt2.commit.message);
+  let div = document.createElement("div");
+  div.classList.add("latestCommitMessage");
+  let span = document.createElement("span");
+  span.innerText = "Latest Commit Message: " + txt2.commit.message;
+  div.appendChild(span);
+  document.body.appendChild(div);
+}
