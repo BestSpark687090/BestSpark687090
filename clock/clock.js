2d30b24d4f675dec27c539c150d4f51ac08954e2
BestSpark687090 <bestspark687090@gmail.com>
2026-08-27 13:51:08 -0500

    fix clock

--- clock/clock.js
+++ clock/clock.js
@@ -1,5 +1,16 @@
 let timesEscapePressed = 0;
 let clockOn = false;
+let iframe = document.createElement("iframe");
+iframe.src = "/clock/index.html";
+iframe.classList.add("clock-frame");
+iframe.style.position = "fixed";
+iframe.style.top = "0";
+iframe.style.left = "0";
+iframe.style.width = "100%";
+iframe.style.height = "100%";
+iframe.style.zIndex = "2147483647";
+iframe.style.display = "none";
+document.body.appendChild(iframe);
 // This probably isn't the best way to get this done, but here's my idea
 document.addEventListener("keyup", function (e) {
   if (e.code == "Escape") {
@@ -9,20 +20,13 @@ document.addEventListener("keyup", function (e) {
     timesEscapePressed = 0;
     // cloak
     if (!clockOn) {
-      let iframe = document.createElement("iframe");
-      iframe.src = "/clock/index.html";
-      iframe.classList.add("clock-frame");
-      iframe.style.position = "fixed";
-      iframe.style.top = "0";
-      iframe.style.left = "0";
-      iframe.style.width = "100%";
-      iframe.style.height = "100%";
-      iframe.style.zIndex = "2147483647";
-      document.body.appendChild(iframe);
+      //document.body.appendChild(iframe);
+      iframe.style.display = "block";
       clockOn = true;
     } else {
       clockOn = false;
-      document.querySelector(".clock-frame").remove();
+      iframe.style.display="none";
+      //document.querySelector(".clock-frame").remove();
     }
   }
 });
